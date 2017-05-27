bdf9980ca659e1c90abd5fa086bf513a6c97e1d9
Ricardo Robles <rroblesgo@gmail.com>
2017-05-27 20:35:33 +0200

    Capitulo 5 Symfony router

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -11,16 +11,45 @@ namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class AdvertController extends Controller
 {
-    public function indexAction()
+    public function indexAction($page)
     {
+        //genera url ruta relativa para         /platform/advert/5
+        $url1 = $this->get('router')->generate(     //abreviado:  generateUrl(
+            'oc_platform_home',
+            array ('id' => 5)
+        );
+
+        //genera url2 ruta absoluta para        http://localhost:8000/platform/advert/5
+        $url2 = $this->get('router')->generate(      //abreviado:  enerateUrl(
+            'oc_platform_home',
+            array('id' => 5),
+            UrlGeneratorInterface::ABSOLUTE_URL);
+
+        //return new Response("La URL del anuncio de id 5 es : ".$url2);
+
+        // para usar path y url desde twig
+        $id = $page;
         $content = $this
             ->get('templating')
             ->render('OCPlatformBundle:Advert:index.html.twig',
-                array('nom' => 'winzou')
+                array('nom' => 'winzou', 'id' => $id)
             );
         return new Response($content);
+
+    }
+
+    public function viewAction($id)
+    {
+        return new Response("Muestra del anuncio de id : ".$id);
+    }
+
+    public function viewSlugAction($slug, $year, $_format)
+    {
+        return new Response("Mostrará el anuncio correspondiente a
+        slug '".$slug."', creado el año ".$year." con el formato ".$_format.".");
     }
 }
