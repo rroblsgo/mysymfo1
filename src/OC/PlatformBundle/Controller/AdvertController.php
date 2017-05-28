99026e424962419b3f99a3bce1960831d686e566
Ricardo Robles <rroblesgo@gmail.com>
2017-05-28 13:48:32 +0200

    Después del cap.6 Los Controladores

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -10,12 +10,77 @@
 namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class AdvertController extends Controller
 {
     public function indexAction($page)
+    {
+        if ($page < 1) {
+            // se  declara una excepción NotFoundHttpException que muestra una pág.404
+            throw new NotFoundHttpException('Page "'.$page.'" inexistente.');
+
+            // ahora recuperaremos la lista de anuncios a mostrar . . .
+
+            // pero por ahora sólo llamamos a la template
+        }
+        return $this->render('OCPlatformBundle:Advert:index.html.twig', array(
+            'page'=>$page
+        ));
+    }
+
+    public function viewAction($id)
+    {
+        // aquí recuperamos el anuncio correspondiente a $id
+        return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+            'id' => $id
+        ));
+        //return $this->redirectToRoute('oc_platform_home');
+    }
+
+    public function addAction(Request $request)
+    {
+        //utilizando mensajes  flashbag y motsrándolos en  view
+//        $session = $request->getSession();
+//        $session->getFlashBag()->add('info', 'Anuncio bien registrado');
+//        $session->getFlashBag()->add('info', 'Si, Si, Anuncio bien registrado');
+//        return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+
+        if($request->isMethod('POST')) {
+            $request->getSession()->getFlashBag()->add('notice', 'Anuncio bien registrado');
+            return $this->redirectToRoute('oc_platform_view', array(
+                'id' => 5
+            ));
+        }
+        return $this->render('OCPlatformBundle:Advert:add.html.twig');
+    }
+
+    public function editAction($id, Request $request)
+    {
+        if($request->isMethod('POST')) {
+            $request->getSession()->getFlashBag()->add('notice', 'Anuncio bien modificado');
+            return $this->redirectToRoute('oc_platform_view', array(
+                'id' => 5
+            ));
+        }
+        return $this->render('OCPlatformBundle:Advert:edit.html.twig');
+    }
+
+    public function deleteAction($id)
+    {
+        return $this->render('OCPlatformBundle:Advert:delete.html.twig');
+    }
+
+    public function viewSlugAction($slug, $year, $_format)
+    {
+        return new Response("Mostrará el anuncio correspondiente a
+        slug '".$slug."', creado el año ".$year." con el formato ".$_format.".");
+    }
+
+    public function novale($page)
     {
         //genera url ruta relativa para         /platform/advert/5
         $url1 = $this->get('router')->generate(     //abreviado:  generateUrl(
@@ -40,16 +105,5 @@ class AdvertController extends Controller
             );
         return new Response($content);
 
-    }
-
-    public function viewAction($id)
-    {
-        return new Response("Muestra del anuncio de id : ".$id);
-    }
-
-    public function viewSlugAction($slug, $year, $_format)
-    {
-        return new Response("Mostrará el anuncio correspondiente a
-        slug '".$slug."', creado el año ".$year." con el formato ".$_format.".");
     }
 }
