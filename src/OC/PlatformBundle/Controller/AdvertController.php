ac63f9574e7c6fb70300524d725cff9b99931e1e
Ricardo Robles <rroblesgo@gmail.com>
2017-05-29 11:11:37 +0200

    Después del tema 11 - Doctrine 2

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -9,25 +9,15 @@
 
 namespace OC\PlatformBundle\Controller;
 
+use OC\PlatformBundle\Entity\Advert;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class AdvertController extends Controller
 {
     public function indexAction($page)
     {
-//        if ($page < 1) {
-//            // se  declara una excepción NotFoundHttpException que muestra una pág.404
-//            throw new NotFoundHttpException('Page "'.$page.'" inexistente.');
-//
-//            // ahora recuperaremos la lista de anuncios a mostrar . . .
-//
-//            // pero por ahora sólo llamamos a la template
-//        }
-
         $listAdverts = array(
           array(
               'title'=>"Búsqueda desarrollador Symfony",
@@ -59,13 +49,14 @@ class AdvertController extends Controller
 
     public function viewAction($id)
     {
-        $advert = array(
-            'title'=>"Búsqueda desarrollador Symfony",
-            'id'=>$id,
-            'author'=>'Alexandre',
-            'content'=>'Buscamos un desarrollador Symfony principiante, de Lyon. blabla...',
-            'date'=>new \DateTime()
-        );
+        $repository=$this->getDoctrine()->getManager()
+            ->getRepository('OCPlatformBundle:Advert');
+
+        $advert=$repository->find($id);
+
+        if(null==$advert) {
+            throw new NotFoundHttpException("El anuncio ".$id. " no existe");
+        }
 
         return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
             'advert' => $advert
@@ -74,17 +65,22 @@ class AdvertController extends Controller
 
     public function addAction(Request $request)
     {
-        //utilizando mensajes  flashbag y motsrándolos en  view
-//        $session = $request->getSession();
-//        $session->getFlashBag()->add('info', 'Anuncio bien registrado');
-//        $session->getFlashBag()->add('info', 'Si, Si, Anuncio bien registrado');
-//        return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+        $advert = new Advert();
+        $advert->setTitle('Búsqueda de un Desarrollador Symfony');
+        $advert->setAuthor('Alexandre');
+        $advert->setContent('Buscamos un Desarrollador Symfony principiante de la zona de Lyon. Bla Bla...');
 
-        if($request->isMethod('POST')) {
+        $em = $this->getDoctrine()->getManager();
+
+        // Etapa 1
+        $em->persist($advert);
+
+        // Etapa 2
+        $em->flush();
+
+        if ($request->isMethod('POST')) {
             $request->getSession()->getFlashBag()->add('notice', 'Anuncio bien registrado');
-            return $this->redirectToRoute('oc_platform_view', array(
-                'id' => 5
-            ));
+            return $this->redirectToRoute('oc_platform_view', array('id' => $advert->getId()));
         }
         return $this->render('OCPlatformBundle:Advert:add.html.twig');
     }
@@ -112,9 +108,9 @@ class AdvertController extends Controller
     public function menuAction($limit)
     {
         $listAdverts = array(
-            array('id'=>2, 'title'=>'Búsqueda desarrollador Symfony'),
-            array('id'=>5, 'title'=>'Misión de Webmaster'),
-            array('id'=>9, 'title'=>'Oferta del puesto Webdesigner')
+            array('id'=>1, 'title'=>'Búsqueda desarrollador Symfony'),
+            array('id'=>2, 'title'=>'Misión de Webmaster'),
+            array('id'=>3, 'title'=>'Oferta del puesto Webdesigner')
         );
 
         return $this->render('OCPlatformBundle:Advert:menu.html.twig', array(
@@ -122,36 +118,4 @@ class AdvertController extends Controller
         ));
     }
 
-    public function viewSlugAction($slug, $year, $_format)
-    {
-        return new Response("Mostrará el anuncio correspondiente a
-        slug '".$slug."', creado el año ".$year." con el formato ".$_format.".");
-    }
-
-    public function novale($page)
-    {
-        //genera url ruta relativa para         /platform/advert/5
-        $url1 = $this->get('router')->generate(     //abreviado:  generateUrl(
-            'oc_platform_home',
-            array ('id' => 5)
-        );
-
-        //genera url2 ruta absoluta para        http://localhost:8000/platform/advert/5
-        $url2 = $this->get('router')->generate(      //abreviado:  enerateUrl(
-            'oc_platform_home',
-            array('id' => 5),
-            UrlGeneratorInterface::ABSOLUTE_URL);
-
-        //return new Response("La URL del anuncio de id 5 es : ".$url2);
-
-        // para usar path y url desde twig
-        $id = $page;
-        $content = $this
-            ->get('templating')
-            ->render('OCPlatformBundle:Advert:index.html.twig',
-                array('nom' => 'winzou', 'id' => $id)
-            );
-        return new Response($content);
-
-    }
 }
